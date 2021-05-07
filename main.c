cb926c06ae4a722cb33ff28048a46d82b744039c
NagiPragalathan <kk2069037@gmail.com>
2021-05-07 15:18:38 +0530

    this is first commit........of this file

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "hider.h"
+int main()
+{
+ int a[20],i,search,n;
+ n=inputd("enter the number of elements : ");
+ for(i=0;i<n;++i)
+ a[i]=inputd("Enter array elements : ");
+
+ search=inputd("Enter element to search: ");
+ for(i=0;i<n;++i)
+ if(a[i]==search)
+ break;
+
+ if(i<n)
+ printf("\n %d is found at position : %d",search, i+1);
+ else
+ printf("\nElement not found");
+
+ return 0;
+}
